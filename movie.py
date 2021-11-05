27d0e93144e6a5beb86a8e26d0f227b2978a2343
Ditthapong lakagul <lakagul.peem@gmail.com>
2021-11-05 20:10:32 +0700

    Move price code

--- movie.py
+++ movie.py
@@ -1,31 +1,3 @@
-from enum import Enum
-
-
-class PriceCode(Enum):
-	new_release = {"price": lambda days: 3.0*days,
-				   "frp": lambda days: days
-		}
-	regular = {"price": lambda days: 2 + ( 1.5 * (days - 2)) if days > 2 else 2,
-				   "frp": lambda days: 1
-		}
-	childrens = {"price": lambda days:1.5 + (1.5 * (days - 3)) if days > 3 else 1.5,
-				   "frp": lambda days: 1
-		}
-
-	def price(self, days: int) -> float:
-		"""Returns the rental price for a given number of days."""
-		pricing = self.value["price"]
-		return pricing(days)
-
-	def point(self, days: int) -> float:
-		"""Returns the rental price for a given number of days."""
-		points = self.value["frp"]
-		return points(days)
-
-
-
-
-
 class Movie:
 	"""
 	A movie available for rent.
