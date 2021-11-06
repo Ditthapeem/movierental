68daa152d6b62287e855e818bae0334c34b5673b
Ditthapong lakagul <lakagul.peem@gmail.com>
2021-11-06 20:25:09 +0700

    Done part 3

--- movie.py
+++ movie.py
@@ -23,3 +23,32 @@ class Movie:
 
 	def __str__(self):
 		return self.title
+
+
+class MovieCatalog:
+
+	def __init__(self):
+		self.movie_data = []
+		file = open("movies.csv", "r")
+		for _ in file:
+			new_movie = file.readline().strip("\n").split(",")
+			movie_data = {'id': new_movie[0], 'title': new_movie[1], 'year': new_movie[2], 'genre': new_movie[3]}
+			self.movie_data.append(movie_data)
+
+	def get_movie(self, title):
+		# Search for title
+		for movie_data in self.movie_data:
+			if title == movie_data["title"]:
+				return Movie(movie_data["title"], movie_data["year"], movie_data["genre"])
+		# If the title doesn't match append in data
+		for id in self.movie_data:
+			new_id = id['id']
+		self.movie_data.append({'id': int(new_id)+1, 'title': title, 'year': "Unknown", 'genre': "Unknown"})
+		return Movie(title, "Unknown", "Unknown")
+
+# 	def debug(self):
+# 		return self.movie_data
+#
+#
+# movie = MovieCatalog()
+# print(movie.get_movie("Mulan").year)
